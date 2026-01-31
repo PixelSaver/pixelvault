bd61a82802a205c4788fc158dfc16b0d63b0beba
PixelSaver <stephen.k@proton.me>
2026-01-30 16:32:00 -0800

    Working on passwordgen ui

--- src/app/ui/features/pw_gen.rs
+++ src/app/ui/features/pw_gen.rs
@@ -1,8 +1,35 @@
 use eframe::egui;
-use crate::app::app::PixelVaultApp;
+use crate::{app::app::PixelVaultApp};
 
 impl PixelVaultApp {
   pub(crate) fn show_password_generator(&mut self, ui: &mut egui::Ui) {
-    ui.label("Password Generator Here");
+    PixelVaultApp::fancy_frame(ui).outer_margin(0.0).show(ui, |ui| {
+      ui.set_width(ui.available_width());
+      
+      ui.add(
+        egui::Label::new(egui::RichText::new("Password Generator Here").heading().size(14.0))
+      );
+      ui.add_enabled(true,
+        egui::TextEdit::singleline(&mut self.pw_gen.generated_password)
+      );
+      egui::Frame::default()
+        .stroke(ui.visuals().widgets.noninteractive.bg_stroke)
+        .fill(ui.visuals().text_edit_bg_color())
+        .inner_margin(3.0)
+        .show(ui, |ui| {
+          // ui.set_width(ui.available_width());
+          ui.set_min_width((self.pw_gen.config.length as f32) * 8.0);
+          ui.label(self.pw_gen.generated_password.clone());
+        });
+      if ui.button("Regenerate password").clicked() {
+        self.pw_gen.generated_password = match self.pw_gen.generate() {
+          Some(password) => password,
+          None => {
+            self.show_error("Failed to generate password");
+            return;
+          },
+        };
+      } 
+    });
   }
 }
--- src/pw_gen.rs
+++ src/pw_gen.rs
@@ -28,10 +28,22 @@ impl Default for PasswordGeneratorConfig {
   }
 }
 
-pub struct PasswordGenerator;
+pub struct PasswordGenerator {
+  pub(super) generated_password: String,
+  pub(super) config: PasswordGeneratorConfig,
+}
+impl Default for PasswordGenerator {
+  fn default() -> Self {
+    Self {
+      generated_password: String::new(),
+      config: PasswordGeneratorConfig::default(),
+    }
+  }
+}
 
 impl PasswordGenerator {
-  pub fn generate(config: &PasswordGeneratorConfig) -> Option<String> {
+  pub fn generate(&mut self) -> Option<String> {
+    let config = &self.config;
     let mut char_set = String::new();
     
     if config.include_uppercase {
